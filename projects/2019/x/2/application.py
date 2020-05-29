f880c8df5c35f4c30474b2741b3292b7cbba9717
Ladislau Nave <ladislau.nave@terra.com.br>
2020-05-28 23:14:26 -0300

    Implement of channel and message creations

--- projects/2019/x/2/application.py
+++ projects/2019/x/2/application.py
@@ -1,6 +1,10 @@
 import os
 
-from flask import Flask, session, redirect, request, render_template, flash
+from datetime import datetime, timezone
+import time
+import locale
+
+from flask import Flask, session, redirect, request, render_template, flash, url_for
 from flask_session import Session
 from flask_socketio import SocketIO, emit
 
@@ -18,8 +22,20 @@ Session(app)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-users=[]
-channels = []
+text_columns_max = 80
+text_rows_number = 10
+text_max_length = text_columns_max * text_rows_number
+texts_config = {}
+texts_config["columns_max"] = text_columns_max
+texts_config["rows_number"] = text_rows_number
+texts_config["max_length"] = text_max_length
+
+messages_config = {}
+messages_config["texts"] = texts_config
+
+
+users_gl=[]
+channels_gl = []
 
 
 @app.route("/")
@@ -47,6 +63,9 @@ def register():
         if not name:
             return apology("must provide name", 403)
 
+        # Get users
+        users = users_gl
+
         # Ensure user does not exists
         for user in users:
             if user["name"] == name:
@@ -55,10 +74,24 @@ def register():
         # Register user
         user = {}
         user["name"] = name
-        users.append(user)
+        users_gl.append(user)
 
         # Remember which user has logged in
+        session.clear()
         session["user"] = user
+
+        # Get locale
+        loc = locale.getlocale(locale.LC_CTYPE)
+        (language_code, encoding) = loc
+        # language_code format returned by getlocale is like 'pt_BR' but 
+        # language_code format required by setlocale is like 'pt-BR' 
+        language_code = language_code.replace("_", "-", 1)
+        session["language_code"] = language_code
+
+        # Get local time zone
+        lt = time.localtime()   # localtime returns tm_gmtoff in seconds
+        gmt_offset = lt.tm_gmtoff
+        session["gmt_offset"] = gmt_offset
 
         # Report message
         flash('You were successfully logged in')
@@ -88,6 +121,9 @@ def login():
         if not name:
             return apology("must provide name", 403)
 
+        # Get users
+        users = users_gl
+
         # Ensure user exists
         found = False
         for user in users:
@@ -99,7 +135,21 @@ def login():
             return apology("user does not exist", 403)
 
         # Remember which user has logged in
+        session.clear()
         session["user"] = user
+
+        # Get locale
+        loc = locale.getlocale(locale.LC_CTYPE)
+        (language_code, encoding) = loc
+        # language_code format returned by getlocale is like 'pt_BR' but 
+        # language_code format required by setlocale is like 'pt-BR' 
+        language_code = language_code.replace("_", "-", 1)
+        session["language_code"] = language_code
+
+        # Get local time zone
+        lt = time.localtime()   # localtime returns tm_gmtoff in seconds
+        gmt_offset = lt.tm_gmtoff
+        session["gmt_offset"] = gmt_offset
 
         # Report message
         flash('You were successfully logged in')
@@ -118,8 +168,20 @@ def login():
 def logout():
     """Log user out"""
 
-    # Forget which user had logged in
-    session["user"] = None
+    session.clear()
+
+    # Redirect user to home page
+    return redirect("/")
+
+
+@app.route("/clear-all")
+@login_required
+def clear_all():
+    """Clear all """
+
+    users_gl = []
+    channels_gl = []
+    session.clear()
 
     # Redirect user to home page
     return redirect("/")
@@ -138,16 +200,20 @@ def unregister():
     elif request.method == "POST":
 
         if request.form.get("confirm"):
+            
+            # For each channel, delete all messages from user
+            user = session["user"]
+            for channel in channels_gl:
+                for message in channel["messages"]:
+                    if message["sender"] == user:
+                        channel["messages"].remove(message)
 
             # Unregister user
-            user = session["user"]
             try:
-                users.remove(user)
+                users_gl.remove(user)
             except:
                 return apology("user does not exist", 403)
-
-            # Forget which user had logged in
-            session["user"] = None
+            session.clear()
 
         # Redirect user to home page
         return redirect("/")
@@ -161,6 +227,9 @@ def unregister():
 @login_required
 def channels_():
     """ Show channels / Create a channel """
+
+    # Get channels
+    channels = channels_gl
 
     # User reached route via GET (as by clicking a link or via redirect)
     if request.method == "GET":
@@ -179,14 +248,19 @@ def channels_():
             if channel["name"] == name:
                 return apology("channel already exists", 403)
 
+        # Set locale for formatting datetime according local language
+        language_code = session["language_code"]
+        locale.setlocale(locale.LC_TIME, language_code)
+
         # Create channel
         channel = {}
         channel["name"] = name
-        channel["owner"] = session["user"]
-        channel["timestamp"] = "28/05/2020 13:00"
+        dt = datetime.now(timezone.utc)
+        channel["timestamp"] = dt.strftime("%a, %d %b %Y %H:%M:%S %Z")
         channel["messages"] = []
-        channels.append(channel)
+        channels_gl.append(channel)
 
+        channels = channels_gl
         return render_template("channels.html", channels=channels)
 
     else:
@@ -198,85 +272,66 @@ def channels_():
 def channel(name):
     """Show channel's messages """
 
+    # Get channels
+    channels = channels_gl
+
     # Ensure channel exists
     for channel in channels:
         if channel["name"] == name:
-            return render_template("channel.html", channel=channel)
-
+            return render_template("channel.html", channel=channel,
+                                   messages_config=messages_config)
     return apology("channel does not exists", 403)
 
-    """
-    # Query database for book
-    book = db.execute("SELECT * FROM books WHERE isbn = :isbn", {"isbn": isbn}).fetchone()
 
-    # Set database timezone
-    gmt_offset = session["gmt_offset"]
-    minutes_offset = (int(abs(gmt_offset) / 60)) % 60
-    h = gmt_offset // 3600
-    hours_offset = ((h > 0) - (h < 0)) * (abs(h) % 24)
-    db.execute(f"SET LOCAL TIME ZONE INTERVAL \'{hours_offset:+03}:{minutes_offset:02}\' \
-                HOUR TO MINUTE")
-    
-    # Query database for book reviews
-    reviews = db.execute("SELECT * FROM reviews WHERE book_id = :book_id ORDER BY timestamp DESC",
-                         {"book_id": book.id}).fetchall()
+@app.route("/message/<string:channel_name>", methods=["GET", "POST"])
+@login_required
+def message(channel_name):
+    """ Send a message """
 
-    reviews_count = len(reviews)
+    # Get channels
+    channels = channels_gl
 
-    # Build reviews data
-    reviews_data = []
-    s = 0
-    ratings_count = 0
-    comments_count = 0
-    for review in reviews:
-        rating = review["rating"]
-        if rating:
-            if rating >= rating_min and rating <= rating_max:
-                ratings_count += 1
-                s += rating
-            else:
-                rating = None
+    # Get channel
+    found = False
+    for channel in channels:
+        if channel["name"] == channel_name:
+            found = True
+            break
+    if not found:
+        return apology("channel does not exists", 403)
 
-        comment = review.comment
-        if comment:
-            if comment.strip():
-                comments_count += 1
-            else:
-                comment = None
+    # User reached route via GET (as by clicking a link or via redirect)
+    if request.method == "GET":
+        return render_template("message.html", channel=channel,
+                               messages_config=messages_config)
 
-        # Query database for book reviewer
-        reviewer = db.execute("SELECT * FROM users WHERE id = :reviewer_id",
-                              {"reviewer_id": review.reviewer_id}).fetchone()
+    # User reached route via POST (as by submitting a form via POST)
+    elif request.method == "POST":
 
-        review_data = {}
-        review_data["reviewer"] = reviewer.name
-        review_data["rating"] = rating
-        review_data["comment"] = comment
-
-        dt = datetime.fromisoformat(str(review.timestamp))
+        # Get message
+        text = request.form.get("text")
+        if text:
+            text_stripped = text.strip()
+        else:
+            text_stripped = None
 
         # Set locale for formatting datetime according local language
         language_code = session["language_code"]
         locale.setlocale(locale.LC_TIME, language_code)
 
-        review_data["datetime"] = dt.strftime("%a, %d %b %Y %H:%M:%S %Z")
+        # Send message
+        if text_stripped:
+            message = {}
+            message["text"] = text_stripped
+            message["sender"] = session["user"]
+            dt = datetime.now(timezone.utc)
+            message["timestamp"] = dt.strftime("%a, %d %b %Y %H:%M:%S %Z")
+            channel_index = channels.index(channel)
+            channels_gl[channel_index]["messages"].append(message)
+            
+        # Redirect user to channel page
+        return redirect(url_for('channel', name=channel['name']))
 
-        reviews_data.append(review_data)
-
-    if ratings_count != 0:
-        average_rating = f"{s / ratings_count:.2f}"
+    # User reached route not via GET neither via POST
     else:
-        average_rating = None
- 
-    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": f"{apikey}", "isbns": f"{isbn}"})
-
-    if res.status_code != 200:
-        raise Exception("ERROR: API request unsuccessful.")
-      
-    review_adds = res.json()["books"][0]
-
-    return render_template("book.html", book=book, average_rating=average_rating,
-                           ratings_count=ratings_count, comments_count=comments_count,
-                           reviews_count=reviews_count, reviews=reviews_data,
-                           review_adds=review_adds, reviews_cfg=reviews_cfg)
-    """
+        return apology("invalid method", 403)
